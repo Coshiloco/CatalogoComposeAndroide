7ccdf726d1ddaacd12476804fe3174e3a4c5b4df
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2023-04-08 13:27:34 +0200

    RecyclerView en Compose con Scaffold Drawer

--- app/src/main/java/com/example/catalogocomposeandroide/Scaffold.kt
+++ app/src/main/java/com/example/catalogocomposeandroide/Scaffold.kt
@@ -4,8 +4,8 @@ package com.example.catalogocomposeandroide
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material.*
@@ -24,29 +24,47 @@ import kotlinx.coroutines.launch
 fun ScaffoldExample() {
     val scaffoldState = rememberScaffoldState()
     val courotineScope = rememberCoroutineScope()
-    Scaffold(topBar = {
-        MyTopAppBar {
-            courotineScope.launch {
-                scaffoldState.snackbarHostState.showSnackbar(
-                    "Has pulsado $it"
-                )
+    Scaffold(
+        topBar = {
+            MyTopAppBar(onClickIcon = {
+                courotineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        "Has pulsado $it"
+                    )
+                }
+            }, onClickDrawer = {
+                courotineScope.launch {
+                    scaffoldState.drawerState.open()
+                }
+            })
+        },
+        scaffoldState = scaffoldState,
+        bottomBar = { MyBottomNavigation() },
+        floatingActionButton = { MyFAB() },
+        floatingActionButtonPosition = FabPosition.Center,
+        isFloatingActionButtonDocked = false,
+        drawerContent = {
+            MyDrawer {
+                courotineScope.launch {
+                    scaffoldState.drawerState.close()
+                }
             }
         }
-    }, scaffoldState = scaffoldState, bottomBar = { MyBottomNavigation()}) {
+    ) {
 
     }
 }
 
 
 @Composable
-fun MyTopAppBar(onClickIcon: (String) -> Unit) {
+fun MyTopAppBar(onClickIcon: (String) -> Unit, onClickDrawer: () -> Unit) {
     TopAppBar(title = { Text(text = "Mi primera toolbar") },
         backgroundColor = Color.Red,
         contentColor = Color.White,
         elevation = 4.dp,
         navigationIcon = {
             IconButton(onClick = { onClickIcon("back") }) {
-                Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "back")
+                Icon(imageVector = Icons.Filled.Menu, contentDescription = "menu")
             }
         },
         actions = {
@@ -66,7 +84,7 @@ fun MyBottomNavigation() {
     var index by rememberSaveable { mutableStateOf(0) }
     BottomNavigation(backgroundColor = Color.Magenta, contentColor = Color.White) {
         BottomNavigationItem(
-            icon = { Icon(imageVector = Icons.Filled.Home, contentDescription = "Home") },
+            icon = { Icon(imageVector = Icons.Filled.Menu, contentDescription = "Menu") },
             label = { Text(text = "Home") },
             selected = index == 0,
             onClick = { index = 0 }
@@ -82,6 +100,48 @@ fun MyBottomNavigation() {
             label = { Text(text = "Person") },
             selected = index == 2,
             onClick = { index = 2 }
+        )
+    }
+}
+
+
+@Composable
+fun MyFAB() {
+    var show by rememberSaveable { mutableStateOf(false) }
+    FloatingActionButton(onClick = { show = true }) {
+        Icon(imageVector = Icons.Filled.Add, contentDescription = "add")
+    }
+    if (show) {
+        MyConfirmationDialog(show = show, onDissmis = { show = false })
+    }
+}
+
+@Composable
+fun MyDrawer(onCloseDrawer: () -> Unit) {
+    Column(modifier = Modifier.padding(10.dp)) {
+        Text(
+            text = "Primera opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp)
+                .clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Segunda opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp)
+                .clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Tercera opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp)
+                .clickable { onCloseDrawer() }
+        )
+        Text(
+            text = "Cuarta opcion", modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp)
+                .clickable { onCloseDrawer() }
         )
     }
 }
